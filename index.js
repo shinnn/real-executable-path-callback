dbcdefa66f139d55d9b7c0b4e3c98a11ac5eecfc
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-31 15:30:40 +0900

    update dependencies and devDependencies
    Highlight: update @shinnn/eslint-config and follow its coding rules, for exmaple tab indent

--- index.js
+++ index.js
@@ -6,43 +6,43 @@ const inspectWithKind = require('inspect-with-kind');
 const isPlainObj = require('is-plain-obj');
 const which = require('which');
 
-const typeErrorMsg = 'Expected an executable name inside the PATH (<string>)';
-const optionErrorMsg = 'Expected an option object to be passed to `node-which` https://www.npmjs.com/package/which (`null` by default)';
+const TYPE_ERROR = 'Expected an executable name inside the PATH (<string>)';
+const OPTION_ERROR = 'Expected an option object to be passed to `node-which` https://www.npmjs.com/package/which (`null` by default)';
 
 module.exports = function realExecutablePathCallback(cmd, options, cb) {
-  if (typeof cmd !== 'string') {
-    throw new TypeError(`${typeErrorMsg}, but got a non-string value ${inspectWithKind(cmd)}.`);
-  }
+	if (typeof cmd !== 'string') {
+		throw new TypeError(`${TYPE_ERROR}, but got a non-string value ${inspectWithKind(cmd)}.`);
+	}
 
-  if (cmd.length === 0) {
-    throw new Error(`${typeErrorMsg.replace(' (<string>)', '')}, but got '' (empty string).`);
-  }
+	if (cmd.length === 0) {
+		throw new Error(`${TYPE_ERROR.replace(' (<string>)', '')}, but got '' (empty string).`);
+	}
 
-  if (cb === undefined) {
-    cb = options;
-    options = {};
-  } else if (options) {
-    if (!isPlainObj(options)) {
-      throw new TypeError(`${optionErrorMsg}, but got ${inspectWithKind(options)}.`);
-    }
+	if (cb === undefined) {
+		cb = options;
+		options = {};
+	} else if (options) {
+		if (!isPlainObj(options)) {
+			throw new TypeError(`${OPTION_ERROR}, but got ${inspectWithKind(options)}.`);
+		}
 
-    if (options.all) {
-      throw new Error('`all` option is not supported.');
-    }
-  } else {
-    options = {};
-  }
+		if (options.all) {
+			throw new Error('`all` option is not supported.');
+		}
+	} else {
+		options = {};
+	}
 
-  if (typeof cb !== 'function') {
-    throw new TypeError(`Expected a callback function, but got ${inspectWithKind(cb)}.`);
-  }
+	if (typeof cb !== 'function') {
+		throw new TypeError(`Expected a callback function, but got ${inspectWithKind(cb)}.`);
+	}
 
-  which(cmd, options, function whichCallback(err, resolvedPaths) {
-    if (err) {
-      cb(err);
-      return;
-    }
+	which(cmd, options, (err, resolvedPaths) => {
+		if (err) {
+			cb(err);
+			return;
+		}
 
-    realpath(resolvedPaths, cb);
-  });
+		realpath(resolvedPaths, cb);
+	});
 };
--- test.js
+++ test.js
@@ -8,74 +8,74 @@ const test = require('tape');
 const isWinFlag = Number(process.platform === 'win32');
 
 test('realExecutablePathCallback()', t => {
-  t.plan(11);
+	t.plan(11);
 
-  process.env.PATH = join(__dirname, 'node_modules', '.bin');
+	process.env.PATH = join(__dirname, 'node_modules', '.bin');
 
-  realExecutablePathCallback('which', (err, filePath) => {
-    t.equal(err, null, 'should not pass any errors when it successfully resolve a path.');
-    t.equal(
-      filePath,
+	realExecutablePathCallback('which', (err, filePath) => {
+		t.equal(err, null, 'should not pass any errors when it successfully resolve a path.');
+		t.equal(
+			filePath,
       join(__dirname, 'node_modules', ['which/bin/which', '.bin/which.CMD'][isWinFlag]), // eslint-disable-line
-      'should resolve an executable path.'
-    );
-  });
+			'should resolve an executable path.'
+		);
+	});
 
-  realExecutablePathCallback('foo', null, ({message}, ...restArgs) => {
-    t.equal(
-      restArgs.length,
-      0,
-      'should only pass one argument to the callback when it fails to resolve a path.'
-    );
+	realExecutablePathCallback('foo', null, ({message}, ...restArgs) => {
+		t.equal(
+			restArgs.length,
+			0,
+			'should only pass one argument to the callback when it fails to resolve a path.'
+		);
 
-    t.equal(
-      message,
-      'not found: foo',
-      'should pass an error to the callback when it fails to resolve a path.'
-    );
-  });
+		t.equal(
+			message,
+			'not found: foo',
+			'should pass an error to the callback when it fails to resolve a path.'
+		);
+	});
 
-  realExecutablePathCallback('which', {path: 'foo'}, ({message}) => {
-    t.equal(
-      message,
-      'not found: which',
-      'should reflect `node-which` options to the result.'
-    );
-  });
+	realExecutablePathCallback('which', {path: 'foo'}, ({message}) => {
+		t.equal(
+			message,
+			'not found: which',
+			'should reflect `node-which` options to the result.'
+		);
+	});
 
-  t.throws(
-    () => realExecutablePathCallback(true, t.fail),
-    /^TypeError.*Expected an executable name.*, but got a non-string value true \(boolean\)\./,
-    'should throw a type error when the first argument is not a string.'
-  );
+	t.throws(
+		() => realExecutablePathCallback(true, t.fail),
+		/^TypeError.*Expected an executable name.*, but got a non-string value true \(boolean\)\./,
+		'should throw a type error when the first argument is not a string.'
+	);
 
-  t.throws(
-    () => realExecutablePathCallback('', undefined, t.fail),
-    /^Error.*Expected an executable name inside the PATH, but got '' \(empty string\)\./,
-    'should throw an error when the first argument is an empty string.'
-  );
+	t.throws(
+		() => realExecutablePathCallback('', undefined, t.fail),
+		/^Error.*Expected an executable name inside the PATH, but got '' \(empty string\)\./,
+		'should throw an error when the first argument is an empty string.'
+	);
 
-  t.throws(
-    () => realExecutablePathCallback('foo', [{}], t.fail),
-    /^TypeError.*passed to `node-which`.*, but got \[ \{} ] \(array\)\./,
-    'should throw a type error when the second argument is neither a function nor an object.'
-  );
+	t.throws(
+		() => realExecutablePathCallback('foo', [{}], t.fail),
+		/^TypeError.*passed to `node-which`.*, but got \[ \{} ] \(array\)\./,
+		'should throw a type error when the second argument is neither a function nor an object.'
+	);
 
-  t.throws(
-    () => realExecutablePathCallback('foo', {all: true}, t.fail),
-    /Error.*`all` option is not supported\./,
-    'should throw an error when it takes `all` option.'
-  );
+	t.throws(
+		() => realExecutablePathCallback('foo', {all: true}, t.fail),
+		/Error.*`all` option is not supported\./,
+		'should throw an error when it takes `all` option.'
+	);
 
-  t.throws(
-    () => realExecutablePathCallback('foo', 1),
-    /^TypeError.*Expected a callback function, but got 1 \(number\)\./,
-    'should throw a type error when the last argument is not a funtion.'
-  );
+	t.throws(
+		() => realExecutablePathCallback('foo', 1),
+		/^TypeError.*Expected a callback function, but got 1 \(number\)\./,
+		'should throw a type error when the last argument is not a funtion.'
+	);
 
-  t.throws(
-    () => realExecutablePathCallback(),
-    /^TypeError.*got a non-string value undefined\./,
-    'should throw a type error when the last argument is not a funtion.'
-  );
+	t.throws(
+		() => realExecutablePathCallback(),
+		/^TypeError.*got a non-string value undefined\./,
+		'should throw a type error when the last argument is not a funtion.'
+	);
 });
